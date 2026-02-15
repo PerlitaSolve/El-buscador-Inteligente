6c94e62632cb363eb1245d4c98d73165e5e339d8
PerlitaSolve <124049201@upq.edu.mx>
2026-02-14 18:28:18 -0600

    Modificación de controller

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -3,8 +3,11 @@ const pool = require('../config/db');
 const buscarProducto = async (req, res) => {
     const Busqueda = req.params.busqueda;
 
+    if (!Busqueda || Busqueda.trim() === '') {
+        return res.status(400).json({ error: 'La búsqueda no puede estar vacía' });
+    }
     try{
-        const row = await pool.query('SELECT * FROM productos where nombre ilike $1', [`%${Busqueda}%`]);
+        const row = await pool.query('SELECT p.nombre, p.descripcion, c.nombre AS categoria, p.precio, p.stock FROM productos p JOIN categoria c ON p.id_categoria = c.id WHERE p.nombre ILIKE $1 OR p.descripcion ILIKE $1', [`%${Busqueda}%`]);
         res.json(row.rows);
     }catch(error){
         console.log(`Error: ${error}`);
@@ -12,11 +15,9 @@ const buscarProducto = async (req, res) => {
     }
 };
 
-const buscarCategoria = async (req, res) => {
-    const Busqueda = req.params.busqueda;
-
+const ListarProductos = async (req, res) => {
     try{
-        const row = await pool.query('SELECT * FROM categoria where nombre ilike $1', [`%${Busqueda}%`]);
+        const row = await pool.query('SELECT p.nombre, p.descripcion, c.nombre AS categoria, p.precio, p.stock FROM productos p JOIN categoria c ON p.id_categoria = c.id');
         res.json(row.rows);
     }catch(error){
         console.log(`Error: ${error}`);
@@ -24,4 +25,4 @@ const buscarCategoria = async (req, res) => {
     }
 };
 
-module.exports ={ buscarProducto, buscarCategoria };
+module.exports ={ buscarProducto, ListarProductos };
--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {buscarCategoria} = require('../controllers/productoController');
-const {buscarProducto} = require('../controllers/productoController');
 
-router.get('/buscarProducto/:busqueda', busquedaProducto);
-router.get('/buscarCategoria/:busqueda', busquedaCategoria);
+const {buscarProducto} = require('../controllers/productoController');
+const {ListarProductos} = require('../controllers/productoController');
+
+router.get('/buscarProducto/:busqueda', buscarProducto);
+router.get('/listarProductos', ListarProductos);
+
 
 module.exports = router;
